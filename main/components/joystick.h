3fe74aa87e848838183816b9dc63e000f8b7a385
Zachral <soderberg.danne@gmail.com>
2023-12-04 20:25:30 +0100

    Added joystick and reading raw values

--- main/components/joystick.h
+++ main/components/joystick.h
@@ -1,8 +1,14 @@
 #ifndef _JOYSTICK_H__
 #define _JOYSTICK_H__
 
+#include "driver/adc.h"
+
 #define HORIZONTAL_JOYSTICK_PIN 34
 #define VERTICAL_JOYSTICK_PIN   35
 #define BUTTON_JOYSTICK_PIN     32
+#define HORIZONTAL_ADC1         ADC1_CHANNEL_6
+#define VERTICAL_ADC1           ADC1_CHANNEL_7
+#define JOYSTICK_BUTTON_ADC1    ADC1_CHANNEL_4
 
+esp_err_t joystick_init(); 
 #endif
--- main/main.c
+++ main/main.c
@@ -2,40 +2,47 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <components/servo.h>
+#include <components/joystick.h>
+#include <driver/adc.h>
+
 
 void app_main(void)
 {
+
+//Setting up servos and related variables 
 mcpwm_cmpr_handle_t wristServo = wrist_servo_init(); 
 mcpwm_cmpr_handle_t shoulderServo = shoulder_servo_init(); 
-int horizontal;
-int vertical;
-int shoulderAngle = 0;
-int wristAngle = 0; 
-
-//Setting servos in center position.
-ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(shoulderServo, run_servos_to_angle(shoulderAngle))); 
+int horizontal, vertical, shoulderAngle = 0, wristAngle = 0; 
+ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(shoulderServo, run_servos_to_angle(shoulderAngle)));//Setting servos in center position. 
 ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(wristServo, run_servos_to_angle(wristAngle))); 
 vTaskDelay(pdMS_TO_TICKS(250)); 
 
+//setting up joystick and related variables
+ESP_ERROR_CHECK(joystick_init()); 
+uint64_t  horizontalReading, verticalReading;  
+
+
 while(1){
 
-   horizontal =
-
-
-    //turning servo arm 45 degrees counter clockwise
-    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(wristServo, run_servos_to_angle(45)));
-    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(shoulderServo, run_servos_to_angle(-45))); 
+    horizontalReading = adc1_get_raw(HORIZONTAL_ADC1);
+    verticalReading = adc1_get_raw(VERTICAL_ADC1); 
+    printf("HORIZONTAL = %lld\nVERTICAL = %lld",horizontalReading,verticalReading); 
     vTaskDelay(pdMS_TO_TICKS(250)); 
 
-    //Setting servos in center position.
-    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(wristServo, run_servos_to_angle(0))); 
-    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(shoulderServo, run_servos_to_angle(0)));
-    vTaskDelay(pdMS_TO_TICKS(250)); 
+    // //turning servo arm 45 degrees counter clockwise
+    // ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(wristServo, run_servos_to_angle(45)));
+    // ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(shoulderServo, run_servos_to_angle(-45))); 
+    // vTaskDelay(pdMS_TO_TICKS(250)); 
 
-    //turning servo arm 45 degrees clockwise
-    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(wristServo, run_servos_to_angle(45))); 
-    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(shoulderServo, run_servos_to_angle(45)));
-    vTaskDelay(pdMS_TO_TICKS(250)); 
+    // //Setting servos in center position.
+    // ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(wristServo, run_servos_to_angle(0))); 
+    // ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(shoulderServo, run_servos_to_angle(0)));
+    // vTaskDelay(pdMS_TO_TICKS(250)); 
+
+    // //turning servo arm 45 degrees clockwise
+    // ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(wristServo, run_servos_to_angle(45))); 
+    // ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(shoulderServo, run_servos_to_angle(45)));
+    // vTaskDelay(pdMS_TO_TICKS(250)); 
     
 }
  
